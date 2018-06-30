d7ebed7d5bea4eced29c028c6e14710802f12be2
Dean Tambling <dean.tambling@gmail.com>
2018-06-30 14:35:57 -0700

    Small styling tweaks

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,8 +5,9 @@ import { GoogleFont } from 'react-typography';
 
 import styles from './index.module.css';
 import typography from '../utils/typography';
-import HashLink from '../utils/HashLink';
-import HashModal from '../utils/HashModal';
+
+import HashLink from '../components/HashLink';
+import HashModal from '../components/HashModal';
 
 export default ({ children }) => (
   <div className={styles.page}>
@@ -16,26 +17,25 @@ export default ({ children }) => (
     </Helmet>
 
     <header className={styles.pageHeader}>
-      <div>
+      <nav>
         <Link to="/"><h1>dean tambling</h1></Link>
         <ul>
           <li><HashLink to="about">about</HashLink></li>
           <li><HashLink to="currently">currently</HashLink></li>
-
         </ul>
-      </div>
+      </nav>
     </header>
 
     <main>
       {children()}
     </main>
 
-      <HashModal path='about'>
-        about
-      </HashModal>
+    <HashModal path='about'>
+      about
+    </HashModal>
 
-      <HashModal path='currently'>
-        currently
-      </HashModal>
+    <HashModal path='currently'>
+      currently
+    </HashModal>
   </div>
 );
