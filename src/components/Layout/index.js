6efe85faf3e3b0bc0792f8feb854a8a86adc55a0
Dean Tambling <dean.tambling@gmail.com>
2019-06-02 15:26:39 -0700

    Add responsiveness

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,24 +11,26 @@ import HashLink from '../HashLink';
 
 export default ({ children }) => (
   <div className={styles.page}>
-    <header className={styles.pageHeader}>
-      <nav>
+    <div className={styles.headerContainer}>
+      <header>
         <img src="https://github.com/tambling.png" alt="Dean Tambling, wearing a ballcap that says 'Cats'" className={styles.image} />
-        <h1><Link to="/">dean tambling</Link></h1>
-        <ul>
-          <li><h6><HashLink to="about">about</HashLink></h6></li>
-          <li><h6><HashLink to="currently">currently</HashLink></h6></li>
-          <li><h6><HashLink to="contact">contact</HashLink></h6></li>
-        </ul>
-      </nav>
-    </header>
+        <nav>
+          <h1><Link to="/">dean tambling</Link></h1>
+          <ul>
+            <li><h6><HashLink to="about">about</HashLink></h6></li>
+            <li><h6><HashLink to="currently">currently</HashLink></h6></li>
+            <li><h6><HashLink to="contact">contact</HashLink></h6></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
 
     <main>
-      {children}
+        {children}
     </main>
 
     <AboutModal />
     <CurrentlyModal />
     <ContactModal />
   </div>
-);
+    );
